8b70331604720b295671c7c3359cd22406ba43ff
Thomas Gelf <thomas@gelf.net>
2016-04-19 12:32:16 +0200

    Oidcache: new main logic in refreshUnresolvedOids

--- lib/Trappola/Oidcache.pm
+++ lib/Trappola/Oidcache.pm
@@ -37,6 +37,50 @@ sub fetchPersistedOids {
     );
 }
 
+sub refreshUnresolvedOids {
+    my $self = shift;
+    my $db = $self->db;
+
+    $db->execute("DELETE FROM trap_oidcache WHERE short_name LIKE '%.%.%'");
+
+    my $query = 'SELECT o.oid FROM ('
+        . ' SELECT DISTINCT oid FROM trap_varbind '
+        . ' UNION SELECT DISTINCT oid FROM trap'
+        # TODO: only unresolved . " UNION SELECT DISTINCT value FROM trap_varbind WHERE type = 'object_id'"
+        . ') o LEFT JOIN trap_oidcache c ON o.oid = c.oid'
+        . ' WHERE c.oid IS NULL';
+    $query = '
+SELECT DISTINCT t.oid FROM trap t LEFT JOIN trap_oidcache c ON t.oid = c.oid WHERE c.oid IS NULL
+UNION
+SELECT DISTINCT tv.oid FROM trap_varbind tv LEFT JOIN trap_oidcache c ON tv.oid = c.oid WHERE c.oid IS NULL
+';
+
+$query = "SELECT t.oid FROM (SELECT DISTINCT oid FROM trap ) t LEFT JOIN trap_oidcache c ON t.oid = c.oid WHERE c.oid IS NULL
+UNION
+SELECT DISTINCT tv.oid FROM trap_varbind tv LEFT JOIN trap_oidcache c ON tv.oid = c.oid WHERE c.oid IS NULL
+UNION
+SELECT tv.oid FROM (SELECT DISTINCT value AS oid FROM trap_varbind WHERE type = 'object_id') tv LEFT JOIN trap_oidcache c ON tv.oid = c.oid WHERE c.oid IS NULL;
+ ";
+
+    my @unresolved = @{ $self->db->fetchCol($query) };
+    my $oid;
+    my %res;
+    foreach $oid (@unresolved) {
+        $res{$oid} = $self->lookup($oid);
+    }
+
+    $self->persist;
+
+    $db->execute(
+        'UPDATE trap t JOIN trap_oidcache c ON t.oid = c.oid'
+        . ' SET t.mib_name = c.mib_name, t.short_name = c.short_name,'
+        . ' t.message = c.description'
+        . " WHERE t.short_name LIKE '%.%.%' AND c.short_name NOT LIKE '%.%.%'"
+    );
+
+    return \%res;
+}
+
 sub lookup {
     my $self = shift;
     my $oid  = shift;

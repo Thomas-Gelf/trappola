4efe1f7571f3c22e75c5d7fd9094019e7178af0f
Thomas Gelf <thomas@gelf.net>
2016-04-19 12:28:26 +0200

    TrapHandler: use Redis, mark legacy code

--- lib/Trappola/TrapHandler.pm
+++ lib/Trappola/TrapHandler.pm
@@ -4,6 +4,7 @@ package Trappola::TrapHandler;
 
 use strict;
 use warnings;
+use Redis;
 use Trappola::Trap;
 use Trappola::Oidcache;
 
@@ -29,12 +30,21 @@ sub oidCache {
 
 sub dispatchMainLoop {
     my $self = shift;
-    my $trap;
+    my ($trap, $list, $value);
+    my $redis = $self->redis;
     while (1) {
-        $self->handleTrap($self->readFromQueue);
+        ($list, $value) = $redis->brpop($redis, 'Trappola::queue', 3);
+        if (not defined $list) {
+            print "Got nothing for 3 secs\n";
+            next;
+        }
+
+        $trap = Trappola::Trap::fromSerialization($value, $self->{'oidcache'});
+        $self->handleTrap($trap);
     }
 }
 
+# Compat only, to be removed soon
 sub dispatchLegacyLoop {
     my $self = shift;
     my $trap;
@@ -44,7 +54,7 @@ sub dispatchLegacyLoop {
 
     while (1) {
         print $fh $self->readFromLegacyQueue->serialize . "\n";
-        # $self->handleTrap($self->readFromLegacyQueue);
+        #$self->handleTrap($self->readFromLegacyQueue);
     }
     close $fh;
 }
@@ -53,10 +63,12 @@ sub handleTrap {
     my $self = shift;
     my $trap = shift;
     my $fh;
-        open($fh, '|/usr/bin/env icingacli trappola trap receive');
+
+    ## TODO: keep it open, eventually re-fork from time to time
+    open($fh, '|/usr/bin/env icingacli trappola trap receive');
     print $fh $trap->serialize . "\n";
     close $fh;
-return;
+    return;
 
     eval {
 
@@ -84,6 +96,7 @@ return;
     close $fh;
 }
 
+# Compat only, to be removed soon
 sub readFromLegacyQueue {
     my $self = shift;
     my $serialized = $self->{'queue'}->pop();
@@ -91,11 +104,52 @@ sub readFromLegacyQueue {
     return Trappola::Trap::fromLegacySerialization($serialized, $self->{'oidcache'});
 }
 
+# Compat only, to be removed soon
 sub readFromQueue {
     my $self = shift;
     my $serialized = $self->{'queue'}->pop();
     # print $serialized;
     return Trappola::Trap::fromSerialization($serialized, $self->{'oidcache'});
+}
+
+## Todo: externalize Redis, this is duplicated code
+sub redis {
+    my $self = shift;
+    unless ($self->{'redis'}) {
+        $self->connectToRedis();
+    }
+
+    return $self->{'redis'};
+}
+
+sub connectToRedis {
+    my $self = shift;
+    eval {
+        my $redisHost = $self->config(
+            'redis',
+            'host',
+            'localhost'
+        );
+
+        my $redisPort = $self->config(
+            'redis',
+            'port',
+            '6379'
+        );
+
+        my $socket = sprintf('%s:%d', $redisHost, $redisPort);
+
+        $self->{'redis'} = Redis->new(
+            server    => $socket,
+            reconnect => 2_592_000, # Give up after 30 days
+            every     => 1_000_000
+        );
+        $self->log('info', 'Successfully connected to Redis at ' . $socket);
+        1;
+    } or do {
+        my $e = $@;
+        $self->log('err', 'Failed to connect to Redis: ' . $e);
+    };
 }
 
 1;
